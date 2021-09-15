af24d50aa1b9404baaa1bed9562d6f83966ace0e
Renan Campos <renan.campos@renan.campos>
2021-09-15 09:42:08 -0300

    partial and apply

--- src/brave/internal_functions.clj
+++ src/brave/internal_functions.clj
@@ -10,7 +10,7 @@
 
 (map titlelize '("list" "list" "list"))
 
-(map #(titlelize (second %)) {:unconfortable-thing "Winking"} )
+(map #(titlelize (second %)) {:unconfortable-thing "Winking"})
 
 
 (cons :zero ["a" "b" :c])
@@ -32,7 +32,7 @@
 
 (defn unify-diet-data
   [human critter]
-  {:human human
+  {:human   human
    :critter critter})
 
 (unify-diet-data human-consumption critter-consumption)
@@ -76,12 +76,12 @@
 (concat [1 2] [3 4])
 
 (def mapper-boladao
-  {:first-name "Renan"
-   :middle-name "Campos"
-   :last-name "Ribeiro"
-   :age nil
+  {:first-name    "Renan"
+   :middle-name   "Campos"
+   :last-name     "Ribeiro"
+   :age           nil
    :year-of-birth 1996
-   :parens {:mom "Sirlene"}})
+   :parens        {:mom "Sirlene"}})
 
 (assoc mapper-boladao :first-name "Naner"
                       :middle-name :Campos
@@ -100,3 +100,60 @@
   ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
 
 (take 10 (even-numbers))
+
+(empty? [])
+(empty? [:no!])
+(empty? [nil])
+
+
+(into {} (map identity {:sunlight-reaction "Glitter"}))
+
+(map identity [:garlic :sesame-oil :fried-eggs])
+(into () (map identity [:garlic :sesame-oil :fried-eggs]))
+
+(into #{} (map identity [:banana-bread :banana-bread]))
+
+(into {:favorite-emotion "Gloomy"} [[:sunlight-reaction "Glitter"]])
+
+(into ["cherry"] '("pine" "pruce"))
+
+(into {:favorite-animal "dog"} {:favorite-food "panqueca" :favorite-dessert "ice cream"})
+
+(conj [0] [1])
+
+(into [0] [1])
+
+(conj [0] 1)
+
+(conj [0] 1 2 3 4 5 6)
+
+(conj {:ue "2"} {:ue "1"})
+
+(conj {:time "Midnight"} [:place "neverland"] {:batata "potato"})
+
+(defn my-conj
+  [target & additions]
+  (into target additions))
+
+(my-conj [0] 1 2 3 4)
+
+(max 102 3 4 55)
+
+(max [1 2 3])
+
+(apply max [1 2 3])
+
+(defn my-into
+  [target additions]
+  (apply conj target additions))
+
+(my-into [1] [2 4])
+
+(def add11 (partial + 11))
+
+(add11 12)
+
+(def add-missing-elements
+  (partial conj ["water" "earth" "air"]))
+
+(add-missing-elements "fire")
