7c05402bb17a706f20c98c40bef0e34c2e7de0df
Renan Campos <renan.campos@renan.campos>
2021-11-01 15:03:36 -0300

    pegame

--- src/brave/peg_thing.clj
+++ src/brave/peg_thing.clj
@@ -1,6 +1,6 @@
-(ns brave.peg-thing
-  (require [clojure.set :as set]
-           (:gen-class)))
+(ns brave.peg-thing)
+(require [clojure.set :as set]
+         (:gen-class))
 
 
 (def peg-board
@@ -29,3 +29,28 @@
     (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
 
+(def tri (tri*))
+
+(take 5 tri)
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, 15, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(triangular? 15)
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(row-tri 3)
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 a row1,
+  positions 2 and 3 in row 2, etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(row-num 5)
