5d1ada8cc90e7bb9bf6fc4b3f0f767c7096d6b09
Renan Campos <renan.campos@renan.campos>
2021-11-11 09:49:50 -0300

    recaptulating"

--- src/brave/core.clj
+++ src/brave/core.clj
@@ -5,3 +5,31 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn cal-age
+  [year-of-birth]
+  (- 2021 year-of-birth))
+
+(cal-age 1996)
+
+
+(def renan (hash-map
+  :name {:first-name  "Renan"
+         :middle-name "Campos"
+         :last-name   "Ribeiro"
+         :full-name   (str (get-in renan [:name :first-name]) " "
+                           (get-in renan [:name :middle-name]) " "
+                           (get-in renan [:name :last-name]))}
+  :year-of-birth 1996
+  :age (cal-age (get renan :year-of-birth))))
+
+(println renan)
+
+(get-in renan [:name :full-name])
+
+(renan :age)
+
+(:age renan)
+
+(:phone-number renan :not-informed)
+
